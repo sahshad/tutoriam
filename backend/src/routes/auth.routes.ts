755e20413a2043895b52ed7329dfc9da00e62702
shahzad <mohmdshahzad2.0@gmail.com>
2025-06-07 12:20:19 +0530

    adding dto in the backend

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -3,16 +3,18 @@ import container from "../di/container";
 import { IAuthController } from "../core/interfaces/controller/IAuthController";
 import { TYPES } from "../di/types";
 import passport from "passport";
+import { validateRequest } from "../middlewares/validate.middleware";
+import { LoginRequestDTO, OtpVerifyRequestDTO, RegisterRequestDTO } from "../dtos/request/auth.request.dto";
 
 const router = express.Router();
 
 const authController = container.get<IAuthController>(TYPES.AuthController);
 
-router.post("/register", authController.register);
-router.post("/verify-otp", authController.verifyOtp);
+router.post("/register", validateRequest(RegisterRequestDTO), authController.register);
+router.post("/verify-otp", validateRequest(OtpVerifyRequestDTO), authController.verifyOtp);
 router.post("/resend-otp", authController.resendOtp);
-router.post("/login", authController.login);
-router.post("/admin/login", authController.adminLogin)
+router.post("/login", validateRequest(LoginRequestDTO), authController.login);
+router.post("/admin/login", validateRequest(LoginRequestDTO), authController.adminLogin);
 router.post("/refresh-token", authController.refreshToken);
 router.post("/logout", authController.logout);
 router.post("/forgot-password", authController.forgotPassword);
