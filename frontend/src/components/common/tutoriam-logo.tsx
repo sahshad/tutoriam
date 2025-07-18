a3f041575f9e427c9ee0fb98e1b81c549c932cc5
shahzad <mohmdshahzad2.0@gmail.com>
2025-07-18 16:06:26 +0530

    sorted users list in admin side and fixed styling issue in the applicaltion review popup

--- frontend/src/components/common/tutoriam-logo.tsx
+++ frontend/src/components/common/tutoriam-logo.tsx
@@ -1,17 +1,18 @@
-import React from "react"
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 interface TutoriamLogoProps {
-  text?: boolean
-  size?: number
+  text?: boolean;
+  size?: number;
 }
 
 const TutoriamLogo: React.FC<TutoriamLogoProps> = ({ text = false, size = 30 }) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleLogoClick = () => {
-    navigate("/")
-  }
+    if (localStorage.getItem("adminLoggedIn")) navigate("/admin/dashboard");
+    else navigate("/");
+  };
 
   return (
     <button
@@ -19,13 +20,7 @@ const TutoriamLogo: React.FC<TutoriamLogoProps> = ({ text = false, size = 30 }) 
       className="cursor-pointer inline-flex items-end text-black dark:text-white w-fit"
       style={{ fontSize: `${size * 0.5}px` }}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 170 120"
-        width={size}
-        height={size}
-        fill="currentColor"
-      >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 170 120" width={size} height={size} fill="currentColor">
         <path d="M.34 0h64.83c24.3.07 41.68 17.07 41.91 41.31q.33 35.47 0 71c-.17 21.28-14.1 37.79-34.17 41-6.3 1-6.37 1-6.37-5.25V47.2c0-6.71 0-6.6-6.61-6.76-9.28-.21-18.67.22-27.8-1.08C14.81 36.88.38 20.06 0 3.16A28 28 0 0 1 .34 0z" />
         <path d="M101.19.86h67.59l-34.25 56.71z" />
       </svg>
@@ -36,7 +31,7 @@ const TutoriamLogo: React.FC<TutoriamLogoProps> = ({ text = false, size = 30 }) 
         </span>
       )}
     </button>
-  )
-}
+  );
+};
 
-export default TutoriamLogo
+export default TutoriamLogo;
